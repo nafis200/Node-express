675ad299872f2985eb4f24c351b09b9c236b90e3
nafis200 <nafisahamed14@gmail.com>
2024-11-16 21:52:04 +0600

    typescript prettier

---
+++ first-project/Readme.md
@@ -0,0 +1,13 @@
+- [Programming hero docs](https://github.com/Apollo-Level2-Web-Dev/Level2-Batch4-PH-University-Server) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [LogRocket docs](https://blog.logrocket.com/linting-typescript-eslint-prettier) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [Mongoose docs](https://mongoosejs.com/) uses [Babel](https://babeljs.io/) for Fast Refresh
+
+it uses future
+
+- [.Eslintrc docs](https://dev.to/shafayat/-express-typescript-eslint-prettiersetup-5fhg) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [Facebook blog](https://www.facebook.com/100004778526351/videos/908806490911910/) uses [Babel](https://babeljs.io/) for Fast Refresh
+
+
+
+- [Lorem Ipsum](https://www.lipsum.com/) uses [Babel](https://babeljs.io/) for Fast Refresh
+
---
+++ first-project/basic.js
@@ -0,0 +1,51 @@
+
+// Mongoose is ODM 
+
+// typescript cors,mongoose, express js
+
+//  npm init -y
+//  npm i express
+
+// npm install mongoose --save
+
+// npm install typescript --save-dev
+
+//  npm i cors
+
+// npm i dotenv
+
+//  npm install --save-dev @types/express
+
+//  tsc -init
+
+//  npm i --save-dev @types/cors
+
+// tsconfig rootdir sec and outdir dist hobe
+
+// DATABASE_URL = mongodb+srv://admin-user:tEulJgxp4g2GNcQA@cluster0.f8w8siu.mongodb.net/first-project?retryWrites=true&w=majority&appName=Cluster0
+
+// add name first-project
+
+// node .\dist\app.js 
+
+
+// Now install EsLint and Prettier
+
+//  add to line tsconfig.json
+
+//   "include": ["src"], // which files to compile
+//   "exclude": ["node_modules"], // which files to skip
+
+// Eslint
+// npm install eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin --save-dev
+// npx eslint --init
+
+// @typescript-eslint/eslint-plugin
+
+//  npm remove eslint beacuse of back 9.14
+
+//   npm i -D eslint@9.14.0
+
+// npm run lint:fix
+
+// npm i -D --exact prettier
