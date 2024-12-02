44aafa6e8ce6135888c22078c848084805b05bb9
nafis200 <nafisahamed14@gmail.com>
2024-12-02 19:36:01 +0600

    001

--- first-project/src/app/modules/user/user-utils.ts
+++ first-project/src/app/modules/user/user-utils.ts
@@ -14,7 +14,7 @@ const findLastStudentId = async()=>{
     .sort({createdAt: -1})
     .lean()
 
-    return lastStudent?.id ? lastStudent.id.substring(6) : undefined
+    return lastStudent?.id ? lastStudent.id : undefined
 }
 
 
@@ -24,7 +24,18 @@ export const generateStudentId = async(payload: TAcademisSemister) => {
 
 //   console.log(await findLastStudentId());
   
-  const currentId = await findLastStudentId() ||(0).toString();
+  let currentId =  (0).toString();
+
+  const lastStudentId = await findLastStudentId()
+  const lastStudentSemesterCode = lastStudentId?.substring(4,6)
+  const lastStudentYear = lastStudentId?.substring(0,4)
+
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year
+  
+  if(lastStudentId && lastStudentSemesterCode === currentSemesterCode && lastStudentYear === currentYear){
+      currentId = lastStudentId.substring(6)
+  }
 
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
