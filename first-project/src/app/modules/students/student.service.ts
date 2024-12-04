c75fc69b0bf94c9f9ead2611050d1fa9ec3739d7
nafis200 <nafisahamed14@gmail.com>
2024-12-04 12:44:26 +0600

    isdeleted

--- first-project/src/app/modules/students/student.service.ts
+++ first-project/src/app/modules/students/student.service.ts
@@ -1,5 +1,8 @@
 // import { TStudent } from './student-interface';
+import mongoose from 'mongoose';
 import { Student } from './student.model';
+import AppError from '../../errors/Apperror';
+import { User } from '../user/user-model';
 
 // const createStudentIntoDB = async (studentData: TStudent) => {
 //   if (await Student.isUserExists(studentData.id)) {
@@ -26,29 +29,58 @@ import { Student } from './student.model';
 // };
 
 const getAllStudentsFromDB = async () => {
-  const result = await Student.find().populate('admissionSemester').populate({
-    path:'academicDepartment',
-    populate:{
-      path:'academicFaculty',
-    },
-  });
+  const result = await Student.find()
+    .populate('admissionSemester')
+    .populate({
+      path: 'academicDepartment',
+      populate: {
+        path: 'academicFaculty',
+      },
+    });
   return result;
 };
 
 const getSingleStudentsFromDB = async (id: string) => {
   // const result = await Student.findOne({ id });
-  const result = await Student.findById(id).populate('admissionSemester').populate({
-    path:'academicDepartment',
-    populate:{
-      path:'academicFaculty',
-    },
-  })
+  const result = await Student.findById(id)
+    .populate('admissionSemester')
+    .populate({
+      path: 'academicDepartment',
+      populate: {
+        path: 'academicFaculty',
+      },
+    });
   return result;
 };
 
 const deleteStudentsFromDB = async (id: string) => {
-  const result = await Student.updateOne({ id }, { isDeleted: true });
-  return result;
+  const session = await mongoose.startSession();
+
+  try {
+
+    session.startTransaction()
+
+    const deletedStudent = await Student.findOneAndUpdate({ id }, { isDeleted: true },{new:true, session});
+
+    if(!deletedStudent){
+      throw new AppError(400,"Failed to delete student")
+    }
+
+    const deletedUser = await User.findOneAndUpdate({ id }, { isDeleted: true },{new:true, session})
+
+    if(!deletedUser){
+      throw new AppError(400,"Failed to delete User")
+    }
+
+    await session.commitTransaction()
+    await session.endSession()
+      
+    return deletedStudent;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+     await session.abortTransaction()
+     await session.endSession()
+  }
 };
 
 export const StudentServices = {
