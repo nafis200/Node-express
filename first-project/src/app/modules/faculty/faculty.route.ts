8a497635de1f6575dfa9eb38e93ed4ab3fa13928
nafis200 <nafisahamed14@gmail.com>
2024-12-07 18:42:22 +0600

    faculty

---
+++ first-project/src/app/modules/faculty/faculty.route.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import { FacultyControllers } from './faculty.controller';
+// import { updateFacultyValidationSchema } from './faculty.validation';
+import ValidateRequest from '../../middleware/validateRequest';
+import { updateFacultyValidationSchema } from './faculty.validation';
+
+const router = express.Router();
+
+router.get('/:id', FacultyControllers.getSingleFaculty);
+
+router.patch(
+  '/:id',
+  ValidateRequest(updateFacultyValidationSchema),
+  FacultyControllers.updateFaculty,
+);
+
+router.delete('/:id', FacultyControllers.deleteFaculty);
+
+router.get('/', FacultyControllers.getAllFaculties);
+
+export const FacultyRoutes = router;
